747a70123e7f246054abf552c67b1366933360c9
MoinJulian <100045739+MoinJulian@users.noreply.github.com>
2023-08-31 12:24:25 +0200

    Updated design

--- src/routes/todos/+page.svelte
+++ src/routes/todos/+page.svelte
@@ -20,8 +20,35 @@
 
 {#if data.todos && data.todos.length > 0}
 	{#each data.todos as todo}
-		<p>{todo.todo}</p>
+		<div>
+			<p>{todo.todo}</p>
+		</div>
 	{/each}
 {:else}
 	<p class="error">No todos available.</p>
 {/if}
+
+<style lang="scss">
+	div {
+		background-color: var(--nav-color);
+		max-width: 40rem;
+		padding: 50px 50px;
+		border-radius: 5px;
+		border: 3px solid var(--border-color);
+		margin-right: auto;
+		margin-bottom: 20px;
+		// button {
+		// 	margin: 10px 0px;
+		// }
+		p {
+			border: 3px solid var(--border-color);
+			max-width: max-content;
+			margin: 0 auto;
+			overflow-wrap: break-word; /* Erzwingt Wortumbrüche */
+		}
+
+		// .hidden {
+		// 	display: none;
+		// }
+	}
+</style>
