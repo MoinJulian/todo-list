3921a55977242ff947c015a6e1c0ef305e38a987
MoinJulian <100045739+MoinJulian@users.noreply.github.com>
2023-08-31 14:20:10 +0200

    Updated Home

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,12 @@
+<svelte:head>
+	<title>ToDo</title>
+</svelte:head>
+
 <h1>ToDo Home</h1>
+
+<p>
+	A ToDo App made with SvelteKit and MongoDB, the todos are stored in the databank and can be
+	deleted and the status of the todo can be toggeled between doen and todo.
+</p>
+
+<p>You first need to create an account and then you can Login in and create Todos.</p>
