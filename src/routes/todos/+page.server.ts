c92de73f90d3312bfe3df825878afb3e4064b7e7
MoinJulian <100045739+MoinJulian@users.noreply.github.com>
2023-08-31 12:08:36 +0200

    Edit Todos

--- src/routes/todos/+page.server.ts
+++ src/routes/todos/+page.server.ts
@@ -2,6 +2,22 @@ import { User_Model } from '$lib/server/models';
 import { v4 as uuidv4 } from 'uuid';
 import type { Actions } from '@sveltejs/kit';
 
+export async function load(event) {
+	const email = event.cookies.get('email');
+	const user = await User_Model.findOne({ 'user.email': email });
+
+	if (!user) {
+		return { status: 400, error: new Error('User could not be found') };
+	} else {
+		const todos = user.todos.map((todo) => {
+			const todoCopy = JSON.parse(JSON.stringify(todo));
+			delete todoCopy._id; // Remove the _id field
+			return todoCopy;
+		});
+		return { todos };
+	}
+}
+
 export const actions: Actions = {
 	default: async (event) => {
 		const email = event.cookies.get('email');
--- src/routes/todos/+page.svelte
+++ src/routes/todos/+page.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
 	import { enhance } from '$app/forms';
-	import type { PageData } from './$types';
 
-	export let data: PageData;
+	export let data;
 </script>
+
+<svelte:head>
+	<title>ToDos</title>
+</svelte:head>
 
 <h1>ToDo Dashboard</h1>
 <h2>Welcome {data.name}</h2>
@@ -14,3 +17,11 @@
 	<input type="text" placeholder="Add a Todo" name="todo" />
 	<button>Submit Todo</button>
 </form>
+
+{#if data.todos && data.todos.length > 0}
+	{#each data.todos as todo}
+		<p>{todo.todo}</p>
+	{/each}
+{:else}
+	<p class="error">No todos available.</p>
+{/if}
